5acd5530d8e66d7b4cac9a2777a120a5b3cead43
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-04-29 08:23:56 +0300

    update styles modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,10 +6,8 @@ const customStyles = {
     left: '50%',
     right: 'auto',
     bottom: 'auto',
-    marginRight: '-50%',
+    marginRight: '-40%',
     transform: 'translate(-50%, -50%)',
-    maxWidth: 'calc(100vw - 48px)',
-    maxHeight: 'calc(100vh - 24px)',
   },
 };
 
